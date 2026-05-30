b75b0b0c517cb797f7a1f7c86e674f0907819eaf
dynasty2016 <admin@knowledgedynasty.info>
2026-05-30 14:28:40 -0400

    Modified abstract_embeddings.py and embeddings .csv files

--- abstract_embeddings.py
+++ abstract_embeddings.py
@@ -14,18 +14,18 @@ csvfile_in = open(INPUT_FILENAME, encoding="cp1252", newline='')
 input_quad = csv.DictReader(csvfile_in)
 
 csvfile_out = open(OUTPUT_FILENAME, "w", encoding="utf8", newline='')
-fieldnames = ['code','description','title','description_embedding']
+fieldnames = ['code','description','title','title_embedding']
 output_quad = csv.DictWriter(csvfile_out, fieldnames=fieldnames)
 output_quad.writeheader()
 
 llm = OpenAI()
 
 for row in input_quad:
-    print(row['description'])
+    print(row['title'])
 
-    description = row['description'].replace('\n', ' ')
-    description_response = llm.embeddings.create(
-        input=description,
+    title = row['title'].replace('\n', ' ')
+    title_response = llm.embeddings.create(
+        input=title,
         model="text-embedding-ada-002"
     )
 
@@ -33,7 +33,7 @@ for row in input_quad:
         'code': row['code'],
         'title': row['title'],
         'description': row['description'], 
-        'description_embedding': description_response.data[0].embedding
+        'title_embedding': title_response.data[0].embedding
     })
 
 csvfile_in.close()
